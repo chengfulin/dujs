c348d5fc772560e4092ca21758b70790c0a069a5
ChengFuLin <chengfulin0806@gmail.com>
2015-04-21 11:42:16 +0800

    Add Scope model for modeling scope name

--- tests/def.js
+++ tests/def.js
@@ -2,6 +2,7 @@
  * Created by chengfulin on 2015/4/16.
  */
 var Def = require('../lib/dujs').Def,
+    Scope = require('../lib/dujs').Scope,
     should = require('should');
 
 describe('Def class', function () {
@@ -9,7 +10,7 @@ describe('Def class', function () {
         var def = new Def(1, [2, 5], 'Program');
         def.from.should.eql(1);
         def.range.should.eql([2, 5]);
-        def.scope.should.eql('Program');
+        def.scope.should.eql(new Scope('Program'));
     });
 
     it('should handle missing parameters', function () {
@@ -27,12 +28,14 @@ describe('Def class', function () {
     });
 
     it('should convert ot string correctly', function () {
+        var defInGlobal = new Def(0, [0, 1], 'Global');
+        defInGlobal.toString().should.eql('Def @n0 @[0,1]_Global');
         var def = new Def(1, [2, 5], 'Program');
         def.toString().should.eql('Def @n1 @[2,5]_Program');
         var defInFun = new Def(2, [5, 9], 'fun');
-        defInFun.toString().should.eql('Def @n2 @[5,9]_function["fun"]');
+        defInFun.toString().should.eql('Def @n2 @[5,9]_Function["fun"]');
         var defInAnonymous = new Def(3, [11, 16], 1);
-        defInAnonymous.toString().should.eql('Def @n3 @[11,16]_anonymousFunction[1]');
+        defInAnonymous.toString().should.eql('Def @n3 @[11,16]_FunctionExpression[1]');
     });
 
     it('should check range property properly', function () {
