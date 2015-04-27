060dc9be431f68e14f6d5b0b02dec0fc45c979a7
ChengFuLin <chengfulin0806@gmail.com>
2015-04-27 17:21:00 +0800

    Bugs fixed and using JSHint

--- tests/dujs/def.js
+++ tests/dujs/def.js
@@ -7,6 +7,7 @@ var Def = require('../../lib/dujs').Def,
     should = require('should');
 
 describe('Def', function () {
+    'use strict';
     describe('constructor', function () {
         it('should construct well', function () {
             var valid = new Def(0, 'object', new Range(0, 1), new Scope('fun'));
@@ -16,16 +17,16 @@ describe('Def', function () {
             valid.getScope().toString().should.eql('Function["fun"]');
 
             (function () {
-                new Def(-1, 'object', new Range(0, 1), new Scope('fun'));
+                var invalid = new Def(-1, 'object', new Range(0, 1), new Scope('fun'));
             }).should.throw('Invalid Def value (from node or type)');
             (function () {
-                new Def(0, 'invalid', new Range(0, 1), new Scope('fun'));
+                var invalid = new Def(0, 'invalid', new Range(0, 1), new Scope('fun'));
             }).should.throw('Invalid Def value (from node or type)');
             (function () {
-                new Def(0, 'object', [1, 0], new Scope('fun'));
+                var invalid = new Def(0, 'object', [1, 0], new Scope('fun'));
             }).should.throw('Invalid Def value (range)');
             (function () {
-                new Def(0, 'object', new Range(0, 1), 'Program');
+                var invalid = new Def(0, 'object', new Range(0, 1), 'Program');
             }).should.throw('Invalid Def value (scope)');
         });
     });
@@ -38,14 +39,18 @@ describe('Def', function () {
             Def.UNDEFINED_TYPE.should.eql('undefined');
 
             /// cannot modified
-            Def.OBJECT_TYPE = 'non-object';
-            Def.OBJECT_TYPE.should.eql('object');
-            Def.FUNCTION_TYPE = 'non-function';
-            Def.FUNCTION_TYPE.should.eql('function');
-            Def.LITERAL_TYPE = 'non-literal';
-            Def.LITERAL_TYPE.should.eql('literal');
-            Def.UNDEFINED_TYPE = 'non-undefined';
-            Def.UNDEFINED_TYPE.should.eql('undefined');
+            (function () {
+                Def.OBJECT_TYPE = 'non-object';
+            }).should.throw();
+            (function () {
+                Def.FUNCTION_TYPE = 'non-function';
+            }).should.throw();
+            (function () {
+                Def.LITERAL_TYPE = 'non-literal';
+            }).should.throw();
+            (function () {
+                Def.UNDEFINED_TYPE = 'non-undefined';
+            }).should.throw();
         });
     });
 
@@ -53,29 +58,29 @@ describe('Def', function () {
         describe('validate', function () {
             it('should validate node and type', function () {
                 (function () {
-                    new Def(-1, 'object', new Range(0, 1), new Scope('fun'));
+                    var invalid = new Def(-1, 'object', new Range(0, 1), new Scope('fun'));
                 }).should.throw('Invalid Def value (from node or type)');
                 (function () {
-                    new Def(0, 'not-a-type', new Range(0, 1), new Scope('fun'));
+                    var invalid = new Def(0, 'not-a-type', new Range(0, 1), new Scope('fun'));
                 }).should.throw('Invalid Def value (from node or type)');
                 (function () {
-                    new Def(0, 'invalid', new Range(0, 1), new Scope('fun'));
+                    var invalid = new Def(0, 'invalid', new Range(0, 1), new Scope('fun'));
                 }).should.throw('Invalid Def value (from node or type)');
 
                 (function () {
-                    new Def(0, 'object', new Range(0, 1), new Scope('fun'));
+                    var valid = new Def(0, 'object', new Range(0, 1), new Scope('fun'));
                 }).should.not.throw();
                 (function () {
-                    new Def(1, 'object', new Range(0, 1), new Scope('fun'));
+                    var valid = new Def(1, 'object', new Range(0, 1), new Scope('fun'));
                 }).should.not.throw();
                 (function () {
-                    new Def(1, 'literal', new Range(0, 1), new Scope('fun'));
+                    var valid = new Def(1, 'literal', new Range(0, 1), new Scope('fun'));
                 }).should.not.throw();
                 (function () {
-                    new Def(1, 'function', new Range(0, 1), new Scope('fun'));
+                    var valid = new Def(1, 'function', new Range(0, 1), new Scope('fun'));
                 }).should.not.throw();
                 (function () {
-                    new Def(1, 'undefined', new Range(0, 1), new Scope('fun'));
+                    var valid = new Def(1, 'undefined', new Range(0, 1), new Scope('fun'));
                 }).should.not.throw();
             });
         });
@@ -85,7 +90,7 @@ describe('Def', function () {
                 var aDef = new Def(0, 'object', [0, 1], Scope.PROGRAM_SCOPE),
                     another = new Def(1, 'literal', [1, 10], new Scope('foo'));
                 aDef.toString().should.eql('Def@n0@[0,1]_Program');
-                another.toString().should.eql('Def@n1[1,10]_Function["foo"]');
+                another.toString().should.eql('Def@n1@[1,10]_Function["foo"]');
             });
         });
     });
