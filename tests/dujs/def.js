e2948e1c94df71e820ac58a8fe3734379275a9df
ChengFuLin <chengfulin0806@gmail.com>
2015-04-27 12:02:34 +0800

    Done testing for Def

--- tests/dujs/def.js
+++ tests/dujs/def.js
@@ -1,81 +1,92 @@
 /**
  * Created by chengfulin on 2015/4/16.
  */
-var Def = require('../../lib/dujs/index').Def,
-    Scope = require('../../lib/dujs/index').Scope,
+var Def = require('../../lib/dujs').Def,
+    Range = require('../../lib/dujs').Range,
+    Scope = require('../../lib/dujs').Scope,
     should = require('should');
 
-describe('Def class', function () {
-    it('should create well with valid data', function () {
-        var def = new Def(1, [2, 5], 'Program');
-        def.from.should.eql(1);
-        def.range.should.eql([2, 5]);
-        def.scope.should.eql(new Scope('Program'));
+describe('Def', function () {
+    describe('constructor', function () {
+        it('should construct well', function () {
+            var valid = new Def(0, 'object', new Range(0, 1), new Scope('fun'));
+            valid.getFromCFGNode().should.eql(0);
+            valid.getType().should.eql('object');
+            valid.getRange().toString().should.eql('[0,1]');
+            valid.getScope().toString().should.eql('Function["fun"]');
+
+            (function () {
+                new Def(-1, 'object', new Range(0, 1), new Scope('fun'));
+            }).should.throw('Invalid Def value (from node or type)');
+            (function () {
+                new Def(0, 'invalid', new Range(0, 1), new Scope('fun'));
+            }).should.throw('Invalid Def value (from node or type)');
+            (function () {
+                new Def(0, 'object', [1, 0], new Scope('fun'));
+            }).should.throw('Invalid Def value (range)');
+            (function () {
+                new Def(0, 'object', new Range(0, 1), 'Program');
+            }).should.throw('Invalid Def value (scope)');
+        });
     });
 
-    it('should handle invalid parameters', function () {
-        (function () {
-            new Def(2, [1, 6]);
-        }).should.throw('Invalid Scope');
+    describe('static constants', function () {
+        it('should have correct constants', function () {
+            Def.OBJECT_TYPE.should.eql('object');
+            Def.FUNCTION_TYPE.should.eql('function');
+            Def.LITERAL_TYPE.should.eql('literal');
+            Def.UNDEFINED_TYPE.should.eql('undefined');
 
-        (function () {
-            new Def(2, [1, 6], 'Global');
-        }).should.throw('Def in "Global" scope not from n0');
-
-        (function () {
-            new Def(2, [0, 0], 'Program');
-        }).should.throw('Invalid range of Def');
-
-        (function () {
-            new Def(2, [1, 0], 'Program');
-        }).should.throw('Invalid range of Def');
-
-        (function () {
-            new Def(2, [0], 'Program');
-        }).should.throw('Invalid range of Def');
-
-        (function () {
-            new Def(2, ['0', '1'], 'Program');
-        }).should.throw('Invalid range of Def');
-
-        (function () {
-            new Def(2, {"0": 0, "1": 1}, 'Program');
-        }).should.throw('Invalid range of Def');
-
-        (function () {
-            new Def(2, null, 'Program');
-        }).should.throw('Invalid range of Def');
-
-        (function () {
-            new Def(-1, [0,1], 'Program');
-        }).should.throw('Def should from the node with valid index');
+            /// cannot modified
+            Def.OBJECT_TYPE = 'non-object';
+            Def.OBJECT_TYPE.should.eql('object');
+            Def.FUNCTION_TYPE = 'non-function';
+            Def.FUNCTION_TYPE.should.eql('function');
+            Def.LITERAL_TYPE = 'non-literal';
+            Def.LITERAL_TYPE.should.eql('literal');
+            Def.UNDEFINED_TYPE = 'non-undefined';
+            Def.UNDEFINED_TYPE.should.eql('undefined');
+        });
     });
 
-    it('should convert ot string correctly', function () {
-        var defInGlobal = new Def(0, [0, 1], 'Global');
-        defInGlobal.toString().should.eql('Def @n0 @[0,1]_Global');
-        var def = new Def(1, [2, 5], 'Program');
-        def.toString().should.eql('Def @n1 @[2,5]_Program');
-        var defInFun = new Def(2, [5, 9], 'fun');
-        defInFun.toString().should.eql('Def @n2 @[5,9]_Function["fun"]');
-        var defInAnonymous = new Def(3, [11, 16], 1);
-        defInAnonymous.toString().should.eql('Def @n3 @[11,16]_FunctionExpression[1]');
-    });
+    describe('methods', function () {
+        describe('validate', function () {
+            it('should validate node and type', function () {
+                (function () {
+                    new Def(-1, 'object', new Range(0, 1), new Scope('fun'));
+                }).should.throw('Invalid Def value (from node or type)');
+                (function () {
+                    new Def(0, 'not-a-type', new Range(0, 1), new Scope('fun'));
+                }).should.throw('Invalid Def value (from node or type)');
+                (function () {
+                    new Def(0, 'invalid', new Range(0, 1), new Scope('fun'));
+                }).should.throw('Invalid Def value (from node or type)');
 
-    it('should check range property properly', function () {
-        var nonArr = {},
-            singleArr = [1],
-            nanArr = ['1', '3'],
-            negativeArr = [-1, 2],
-            reverseArr = [2, 0],
-            sameArr = [1, 1],
-            validArr = [0, 10];
-        should(Def.isRange(nonArr)).be.false;
-        should(Def.isRange(singleArr)).be.false;
-        should(Def.isRange(nanArr)).be.false;
-        should(Def.isRange(negativeArr)).be.false;
-        should(Def.isRange(reverseArr)).be.false;
-        should(Def.isRange(sameArr)).be.false;
-        should(Def.isRange(validArr)).be.true;
+                (function () {
+                    new Def(0, 'object', new Range(0, 1), new Scope('fun'));
+                }).should.not.throw();
+                (function () {
+                    new Def(1, 'object', new Range(0, 1), new Scope('fun'));
+                }).should.not.throw();
+                (function () {
+                    new Def(1, 'literal', new Range(0, 1), new Scope('fun'));
+                }).should.not.throw();
+                (function () {
+                    new Def(1, 'function', new Range(0, 1), new Scope('fun'));
+                }).should.not.throw();
+                (function () {
+                    new Def(1, 'undefined', new Range(0, 1), new Scope('fun'));
+                }).should.not.throw();
+            });
+        });
+
+        describe('toString', function () {
+            it('should convert to string correctly', function () {
+                var aDef = new Def(0, 'object', [0, 1], Scope.PROGRAM_SCOPE),
+                    another = new Def(1, 'literal', [1, 10], new Scope('foo'));
+                aDef.toString().should.eql('Def@n0@[0,1]_Program');
+                another.toString().should.eql('Def@n1[1,10]_Function["foo"]');
+            });
+        });
     });
 });
