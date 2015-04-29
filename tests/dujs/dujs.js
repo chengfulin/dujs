292ad2cf3671ae5b3aba55bebaa7dd4e9c0fa475
ChengFuLin <chengfulin0806@gmail.com>
2015-04-29 22:07:10 +0800

    Fix bug of missing params

--- tests/dujs/dujs.js
+++ tests/dujs/dujs.js
@@ -14,10 +14,10 @@ describe('DuJs', function () {
         });
         describe('buildCFGWrapperTree', function () {
             it('should build the tree well', function () {
-                var code = 'function foo() {\n' +
+                var code = 'function foo(x) {\n' +
                                 'function inner() {}\n' +
                             '}\n' +
-                            'var foo2 = function () {};',
+                            'var foo2 = function (a, b) {};',
                     rootScope,
                     childScope1,
                     childScope2,
@@ -48,14 +48,37 @@ describe('DuJs', function () {
                 childScope1.getParent().getScope().toString().should.eql('Program');
                 childScope1.getChildren().size.should.eql(1);
                 should(childScope1.getChildren().has(descendentScope.getDef())).eql(true);
+                childScope1.getParams().size.should.eql(1);
+                should(childScope1.getParams().has('x')).eql(true);
+                childScope1.getScopeVars().size.should.eql(2);
+                should(childScope1.getScopeVars().has('inner')).eql(true);
+                should(childScope1.getScopeVars().has('x')).eql(true);
+                var childScope1EntryRDs = childScope1.getReachDefinitions().get(childScope1.getCFG()[0]),
+                    innerVar = childScope1.getScopeVars().get('inner'),
+                    xVar = childScope1.getScopeVars().get('x');
+                should.exist(childScope1EntryRDs.get(innerVar));
+                should.exist(childScope1EntryRDs.get(xVar));
+                /// params don't get the def yet
+                childScope1EntryRDs.get(xVar).values().length.should.eql(0);
 
                 should.exist(childScope2.getParent());
                 childScope2.getParent().getScope().toString().should.eql('Program');
                 childScope2.getChildren().size.should.eql(0);
+                childScope2.getParams().size.should.eql(2);
+                should(childScope2.getParams().has('a')).eql(true);
+                should(childScope2.getParams().has('b')).eql(true);
+                var childScope2EntryRDs = childScope2.getReachDefinitions().get(childScope2.getCFG()[0]),
+                    aVar = childScope2.getScopeVars().get('a'),
+                    bVar = childScope2.getScopeVars().get('b');
+                should.exist(childScope2EntryRDs.get(aVar));
+                should.exist(childScope2EntryRDs.get(bVar));
+                /// params don't get the def yet
+                childScope2EntryRDs.get(aVar).values().length.should.eql(0);
 
                 should.exist(descendentScope.getParent());
                 descendentScope.getParent().getScope().toString().should.eql('Function["foo"]');
                 descendentScope.getChildren().size.should.eql(0);
+                descendentScope.getParams().size.should.eql(0);
             });
         });
     });
