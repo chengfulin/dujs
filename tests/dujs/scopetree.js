d5ef78f5df49a22e21549eec173b3f4aadd7db06
ChengFuLin <chengfulin0806@gmail.com>
2015-05-05 21:57:55 +0800

    Add API for checking and getting the definitions of a variable reaching in and parameters should not have definition before called

--- tests/dujs/scopetree.js
+++ tests/dujs/scopetree.js
@@ -1,17 +1,13 @@
 /**
  * Created by chengfulin on 2015/4/29.
  */
-var DuJs = require('../../lib/dujs').DuJs,
+var FunctionScopeTree = require('../../lib/dujs').FunctionScopeTree,
     CFGExt = require('../../lib/dujs').CFGExt,
     should = require('should');
 
-describe('DuJs', function () {
+describe('FunctionScopeTree', function () {
     'use strict';
     describe('methods', function () {
-        var dujs;
-        beforeEach(function () {
-            dujs = new DuJs();
-        });
         describe('buildCFGWrapperTree', function () {
             it('should build the tree well', function () {
                 var code = 'function foo(x) {\n' +
@@ -21,14 +17,15 @@ describe('DuJs', function () {
                     rootScope,
                     childScope1,
                     childScope2,
-                    descendentScope;
-                dujs.buildCFGWrapperTree(CFGExt.parseAST(code));
-                dujs.getFunctionScopes().length.should.eql(4);
+                    descendentScope,
+                    tree;
+                tree = new FunctionScopeTree(CFGExt.parseAST(code));
+                tree.getFunctionScopes().length.should.eql(4);
 
-                rootScope = dujs.getRoot();
-                childScope1 = dujs.getFunctionScopes()[1];
-                childScope2 = dujs.getFunctionScopes()[3];
-                descendentScope = dujs.getFunctionScopes()[2];
+                rootScope = tree.getRoot();
+                childScope1 = tree.getFunctionScopes()[1];
+                childScope2 = tree.getFunctionScopes()[3];
+                descendentScope = tree.getFunctionScopes()[2];
 
                 /// Program
                 ///     + Function["foo"]
@@ -41,44 +38,42 @@ describe('DuJs', function () {
 
                 should.not.exist(rootScope.getParent());
                 rootScope.getChildren().size.should.eql(2);
-                should(rootScope.getChildren().has(childScope1.getDef())).eql(true);
-                should(rootScope.getChildren().has(childScope2.getDef())).eql(true);
+                should(rootScope.getChildren().has(childScope1.getRange().toString())).eql(true);
+                should(rootScope.getChildren().has(childScope2.getRange().toString())).eql(true);
 
                 should.exist(childScope1.getParent());
                 childScope1.getParent().getScope().toString().should.eql('Program');
                 childScope1.getChildren().size.should.eql(1);
-                should(childScope1.getChildren().has(descendentScope.getDef())).eql(true);
-                childScope1.getParams().size.should.eql(1);
-                should(childScope1.getParams().has('x')).eql(true);
+                should(childScope1.getChildren().has(descendentScope.getRange().toString())).eql(true);
+                childScope1.getFunctionParams().size.should.eql(1);
+                should(childScope1.getFunctionParams().has('x')).eql(true);
                 childScope1.getScopeVars().size.should.eql(2);
                 should(childScope1.getScopeVars().has('inner')).eql(true);
                 should(childScope1.getScopeVars().has('x')).eql(true);
-                var childScope1EntryRDs = childScope1.getReachDefinitions().get(childScope1.getCFG()[0]),
+                var childScope1EntryNode = childScope1.getCFG()[0],
                     innerVar = childScope1.getScopeVars().get('inner'),
                     xVar = childScope1.getScopeVars().get('x');
-                should.exist(childScope1EntryRDs.get(innerVar));
-                should.exist(childScope1EntryRDs.get(xVar));
-                /// params don't get the def yet
-                childScope1EntryRDs.get(xVar).values().length.should.eql(0);
+                should(childScope1.doesVarReachIn(childScope1EntryNode, innerVar)).eql(true);
+                should(childScope1.doesVarReachIn(childScope1EntryNode, xVar)).eql(false);
 
                 should.exist(childScope2.getParent());
                 childScope2.getParent().getScope().toString().should.eql('Program');
                 childScope2.getChildren().size.should.eql(0);
-                childScope2.getParams().size.should.eql(2);
-                should(childScope2.getParams().has('a')).eql(true);
-                should(childScope2.getParams().has('b')).eql(true);
-                var childScope2EntryRDs = childScope2.getReachDefinitions().get(childScope2.getCFG()[0]),
+                childScope2.getFunctionParams().size.should.eql(2);
+                should(childScope2.getFunctionParams().has('a')).eql(true);
+                should(childScope2.getFunctionParams().has('b')).eql(true);
+                var childScope2EntryNode = childScope2.getCFG()[0],
                     aVar = childScope2.getScopeVars().get('a'),
                     bVar = childScope2.getScopeVars().get('b');
-                should.exist(childScope2EntryRDs.get(aVar));
-                should.exist(childScope2EntryRDs.get(bVar));
+                should(childScope2.doesVarReachIn(childScope2EntryNode, aVar)).eql(false);
+                should(childScope2.doesVarReachIn(childScope2EntryNode, bVar)).eql(false);
                 /// params don't get the def yet
-                childScope2EntryRDs.get(aVar).values().length.should.eql(0);
+                childScope2.getVarDefsReachIn(childScope2, aVar).values().length.should.eql(0);
 
                 should.exist(descendentScope.getParent());
                 descendentScope.getParent().getScope().toString().should.eql('Function["foo"]');
                 descendentScope.getChildren().size.should.eql(0);
-                descendentScope.getParams().size.should.eql(0);
+                descendentScope.getFunctionParams().size.should.eql(0);
             });
         });
     });
