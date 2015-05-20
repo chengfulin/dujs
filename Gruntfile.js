e16f6889af9581232a22b978bf8dcd5fe6b3515b
ChengFuLin <chengfulin0806@gmail.com>
2015-05-20 18:54:08 +0800

    Use Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ChengFuLin on 2015/5/20.
+ */
+module.exports = function (grunt) {
+    "use strict";
+    grunt.initConfig({
+        strip_code: {
+            options: {
+                start_comment: 'start-test-block',
+                end_comment: 'end-test-block'
+            },
+            src: ['lib/**/*.js']
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    require: 'coverage/blanket'
+                },
+                src: ['tests/dujs/*.js', 'tests/analyses/*.js', 'tests/esgraph/*.js']
+            },
+            coverage: {
+                options: {
+                    reporter: 'html-cov',
+                    quiet: true,
+                    captureFile: 'coverage/coverage.html'
+                },
+                src: ['tests/dujs/*.js', 'tests/analyses/*.js', 'tests/esgraph/*.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-strip-code');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('default', ['strip_code', 'mochaTest']);
+};
