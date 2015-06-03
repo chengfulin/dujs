145f24ca31250c06c22dc3d2cc51f7fe5b318067
ChengFuLin <chengfulin0806@gmail.com>
2015-06-03 12:50:46 +0800

    Refactor and tested to the method setInitVars

--- tests/dujs/scopewrapper.js
+++ tests/dujs/scopewrapper.js
@@ -8,36 +8,16 @@ var ScopeWrapper = require('../../lib/dujs').ScopeWrapper,
     //Range = require('../../lib/dujs').Range,
     //CfgExt = require('../../lib/dujs').CFGExt,
     //Map = require('core-js/es6/map'),
-    //Set = require('../../lib/analyses').Set,
-    //vardefFactory = require('../../lib/dujs').factoryVarDef,
-    //varFactory = require('../../lib/dujs').factoryVar,
-    FlowNode = require('../../lib/esgraph').FlowNode,
+    Set = require('../../lib/analyses').Set,
+    vardefFactory = require('../../lib/dujs').factoryVarDef,
+    defFactory = require('../../lib/dujs').factoryDef,
+    varFactory = require('../../lib/dujs').factoryVar,
+    //FlowNode = require('../../lib/esgraph').FlowNode,
+    factoryFlowNode = require('../../lib/esgraph').factoryFlowNode,
     should = require('should');
 
 describe('ScopeWrapper', function () {
     'use strict';
-    var monkVar = (function () {
-            function Var(name) {
-                this.name = name;
-            }
-            return {
-                create: function (name) {
-                    return new Var(name);
-                }
-            };
-        }()),
-        monkWrapper = (function () {
-            function ScopeWrapper(cfg, scope) {
-                this.cfg = cfg;
-                this.scope = scope;
-            }
-            return {
-                create: function (cfg, scope) {
-                    return new ScopeWrapper(cfg, scope);
-                }
-            };
-        } ());
-
     describe('Static Methods', function () {
         describe('isValidParent', function () {
             it('should return false as the input is not a ScopeWrapper or null', function () {
@@ -46,8 +26,8 @@ describe('ScopeWrapper', function () {
             });
 
             it('should return true as the input is a ScopeWrapper or null', function () {
-                var node1 = new FlowNode(FlowNode.NORMAL_NODE_TYPE),
-                    node2 = new FlowNode(FlowNode.EXIT_NODE_TYPE);
+                var node1 = factoryFlowNode.createNormalNode(),
+                    node2 = factoryFlowNode.createExitNode();
                 ScopeWrapper.isValidParent(new ScopeWrapper([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE)).should.eql(true);
                 ScopeWrapper.isValidParent(null).should.eql(true);
             });
@@ -61,8 +41,8 @@ describe('ScopeWrapper', function () {
             });
 
             it('should return true as the input is a ScopeWapper', function () {
-                var node1 = new FlowNode(FlowNode.NORMAL_NODE_TYPE),
-                    node2 = new FlowNode(FlowNode.EXIT_NODE_TYPE);
+                var node1 = factoryFlowNode.createNormalNode(),
+                    node2 = factoryFlowNode.createExitNode();
                 ScopeWrapper.isScopeWrapper(new ScopeWrapper([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE));
             });
         });
@@ -79,8 +59,8 @@ describe('ScopeWrapper', function () {
             });
 
             it('should throw as the Scope value is invalid', function () {
-                var node1 = new FlowNode(FlowNode.CALL_NODE_TYPE),
-                    node2 = new FlowNode(FlowNode.CALL_RETURN_NODE_TYPE);
+                var node1 = factoryFlowNode.createCallNode(),
+                    node2 = factoryFlowNode.createCallReturnNode();
                 should(function () {
                     ScopeWrapper.validate([node1, node2, [node1, node2]], 'InvalidScope');
                 }).throw('Invalid Scope value for a ScopeWrapper');
@@ -93,8 +73,8 @@ describe('ScopeWrapper', function () {
             });
 
             it('should not throw as the inputs are valid', function () {
-                var node1 = new FlowNode(FlowNode.CALL_NODE_TYPE),
-                    node2 = new FlowNode(FlowNode.CALL_RETURN_NODE_TYPE);
+                var node1 = factoryFlowNode.createCallNode(),
+                    node2 = factoryFlowNode.createCallReturnNode();
                 should(function () {
                     ScopeWrapper.validate([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE);
                 }).not.throw();
@@ -119,8 +99,8 @@ describe('ScopeWrapper', function () {
             });
 
             it('should not throw as the input is a ScopeWrapper', function () {
-                var node1 = new FlowNode(FlowNode.CALL_NODE_TYPE),
-                    node2 = new FlowNode(FlowNode.CALL_RETURN_NODE_TYPE);
+                var node1 = factoryFlowNode.createCallNode(),
+                    node2 = factoryFlowNode.createCallReturnNode();
                 should(function () {
                     ScopeWrapper.validateType(new  ScopeWrapper([node1, node2, [node1, node2]], Scope.GLOBAL_SCOPE));
                 }).not.throw();
@@ -130,8 +110,8 @@ describe('ScopeWrapper', function () {
 
     describe('Constructor', function () {
         it('should construct with value well', function () {
-            var node1 = new FlowNode(FlowNode.NORMAL_NODE_TYPE),
-                node2 = new FlowNode(FlowNode.CALL_NODE_TYPE);
+            var node1 = factoryFlowNode.createNormalNode(),
+                node2 = factoryFlowNode.createCallNode();
             var wrapper = new ScopeWrapper([node1, node2, [node1, node2]], Scope.GLOBAL_SCOPE);
 
             wrapper._testonly_._cfg.length.should.eql(3);
@@ -152,8 +132,8 @@ describe('ScopeWrapper', function () {
     describe('Properties', function () {
         var node1, node2, wrapper;
         beforeEach(function () {
-            node1 = new FlowNode(FlowNode.ENTRY_NODE_TYPE);
-            node2 = new FlowNode(FlowNode.EXIT_NODE_TYPE);
+            node1 = factoryFlowNode.createEntryNode();
+            node2 = factoryFlowNode.createExitNode();
             wrapper = new ScopeWrapper([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE);
         });
 
@@ -190,8 +170,8 @@ describe('ScopeWrapper', function () {
         describe('parent', function () {
             var node3, node4, parentWrapper;
             beforeEach(function () {
-                node3 = new FlowNode(FlowNode.CALL_NODE_TYPE);
-                node4 = new FlowNode(FlowNode.CALL_RETURN_NODE_TYPE);
+                node3 = factoryFlowNode.createCallNode();
+                node4 = factoryFlowNode.createCallReturnNode();
                 parentWrapper = new ScopeWrapper([node3, node4, [node3, node4]], Scope.GLOBAL_SCOPE);
             });
 
@@ -216,7 +196,7 @@ describe('ScopeWrapper', function () {
         describe('def', function () {
             var def, node;
             beforeEach(function () {
-                node = new FlowNode(FlowNode.NORMAL_NODE_TYPE);
+                node = factoryFlowNode.createNormalNode();
                 node.cfgId = 0;
                 def = new Def(node, Def.LITERAL_TYPE, [0,1], Scope.GLOBAL_SCOPE);
             });
@@ -238,11 +218,11 @@ describe('ScopeWrapper', function () {
 
         describe('vars', function () {
             it('should retrieve the value correctly', function () {
-                var monkedVar = monkVar.create('tmp');
-                wrapper._testonly_._vars.set(monkedVar.name, monkedVar);
+                var tmp = varFactory.create('tmp', [0,1], Scope.PROGRAM_SCOPE);
+                wrapper._testonly_._vars.set(tmp.name, tmp);
                 wrapper.vars.size.should.eql(1);
                 should.exist(wrapper.vars.get('tmp'));
-                wrapper.vars.get('tmp').should.eql(monkedVar);
+                wrapper.vars.get('tmp').should.eql(tmp);
             });
 
             it('should not modified', function () {
@@ -254,11 +234,11 @@ describe('ScopeWrapper', function () {
 
         describe('params', function () {
             it('should retrieve the value correctly', function () {
-                var monkedParam = monkVar.create('param');
-                wrapper._testonly_._params.set(monkedParam.name, monkedParam);
+                var param = varFactory.create('param', [0,1], Scope.PROGRAM_SCOPE);
+                wrapper._testonly_._params.set(param.name, param);
                 wrapper.params.size.should.eql(1);
                 should.exist(wrapper.params.get('param'));
-                wrapper.params.get('param').should.eql(monkedParam);
+                wrapper.params.get('param').should.eql(param);
             });
 
             it('should not modified', function () {
@@ -270,12 +250,122 @@ describe('ScopeWrapper', function () {
 
         describe('children', function () {
             it('should retrieve the value correctly', function () {
-                var node1 = new FlowNode(FlowNode.NORMAL_NODE_TYPE),
-                    node2 = new FlowNode(FlowNode.NORMAL_NODE_TYPE),
-                    monkedWrapper = monkWrapper.create([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE);
-                wrapper._testonly_._children.push(monkedWrapper);
+                var node1 = factoryFlowNode.createNormalNode(),
+                    node2 = factoryFlowNode.createNormalNode(),
+                    childWrapper = new ScopeWrapper([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE);
+                wrapper._testonly_._children.push(childWrapper);
                 wrapper.children.length.should.eql(1);
-                wrapper.children[0].should.eql(monkedWrapper);
+                wrapper.children[0].should.eql(childWrapper);
+            });
+        });
+    });
+
+    describe('Methods', function () {
+        describe('hasVarWithName', function () {
+            var node1, node2, cfg, wrapper, var1;
+            beforeEach(function () {
+                node1 = factoryFlowNode.createNormalNode();
+                node2 = factoryFlowNode.createEntryNode();
+                cfg = [node1, node2, [node1, node2]];
+                wrapper = new ScopeWrapper(cfg, Scope.PROGRAM_SCOPE);
+                var1 = varFactory.create('var1', [0,1], Scope.PROGRAM_SCOPE);
+
+                wrapper._testonly_._vars.set('var1', var1);
+            });
+
+            it('should return as the name is valid', function () {
+                wrapper.hasVarWithName('var1').should.eql(true);
+            });
+
+            it('should return false as the name is invalid', function () {
+                wrapper.hasVarWithName('var2').should.eql(false);
+                wrapper.hasVarWithName().should.eql(false);
+                wrapper.hasVarWithName({}).should.eql(false);
+            });
+        });
+
+        describe('getVarByName', function () {
+            var node1, node2, node3, node4, childWrapper, parentWrapper, var1, var2;
+            beforeEach(function () {
+                node1 = factoryFlowNode.createEntryNode();
+                node2 = factoryFlowNode.createExitNode();
+                node3 = factoryFlowNode.createCallNode();
+                node4 = factoryFlowNode.createCallReturnNode();
+
+                childWrapper = new ScopeWrapper([node1, node2, [node1, node2]], Scope.PROGRAM_SCOPE);
+                parentWrapper = new ScopeWrapper([node3, node4, [node3, node4]], Scope.GLOBAL_SCOPE);
+
+                var1 = varFactory.create('var1', [1,2], Scope.PROGRAM_SCOPE);
+                var2 = varFactory.create('var2', [0,1], Scope.GLOBAL_SCOPE);
+
+                childWrapper._testonly_._vars.set('var1', var1);
+                parentWrapper._testonly_._vars.set('var2', var2);
+
+                childWrapper._testonly_._parent = parentWrapper;
+                parentWrapper._testonly_._children.push(childWrapper);
+            });
+
+            it('should support to find the var in the current scope', function () {
+                should.exist(childWrapper.getVarByName('var1'));
+                childWrapper.getVarByName('var1').should.eql(var1);
+            });
+
+            it('should support to find the var in the outer scope', function () {
+                should.exist(childWrapper.getVarByName('var2'));
+                childWrapper.getVarByName('var2').should.eql(var2);
+            });
+        });
+
+        describe('setInitVars', function () {
+            var node1, node2, cfg, wrapper, var1, var2;
+            beforeEach(function () {
+                node1 = factoryFlowNode.createEntryNode();
+                node1.cfgId = 0;
+                node2 = factoryFlowNode.createExitNode();
+                node2.cfgId = 1;
+                cfg = [node1, node2, [node1, node2]];
+                wrapper = new ScopeWrapper(cfg, Scope.PROGRAM_SCOPE);
+                var1 = varFactory.createGlobalVar('var1');
+                var2 = varFactory.createGlobalVar('var2');
+                varFactory.resetGlobalsCounter();
+            });
+
+            it('should set initial Vars with array of Vars well', function () {
+                var vars = [var1, var2];
+                wrapper.setInitVars(vars);
+                wrapper._testonly_._vars.size.should.eql(2);
+                wrapper._testonly_._vars.get('var1').should.eql(var1);
+                wrapper._testonly_._vars.get('var2').should.eql(var2);
+                wrapper._testonly_._initialVars.size.should.eql(2);
+                wrapper._testonly_._initialVars.get('var1').should.eql(var1);
+                wrapper._testonly_._initialVars.get('var2').should.eql(var2);
+            });
+
+            it('should set initial Vars with set of Vars well', function () {
+                var vars = new Set([var1, var2]);
+                wrapper.setInitVars(vars);
+                wrapper._testonly_._vars.size.should.eql(2);
+                wrapper._testonly_._vars.get('var1').should.eql(var1);
+                wrapper._testonly_._vars.get('var2').should.eql(var2);
+                wrapper._testonly_._initialVars.size.should.eql(2);
+                wrapper._testonly_._initialVars.get('var1').should.eql(var1);
+                wrapper._testonly_._initialVars.get('var2').should.eql(var2);
+            });
+
+            it('should support to set initial Vars with VarDefs', function () {
+                var def1 = defFactory.createLiteralDef(node1, var1._testonly_._range, var1._testonly_._scope),
+                    def2 = defFactory.createLiteralDef(node2, var2._testonly_._range, var2._testonly_._scope),
+                    vardef1 = vardefFactory.create(var1, def1),
+                    vardef2 = vardefFactory.create(var2, def2),
+                    initials = new Set([vardef1, vardef2]);
+
+                wrapper.setInitVars(initials);
+                wrapper._testonly_._vars.size.should.eql(2);
+                wrapper._testonly_._vars.get('var1').should.eql(var1);
+                wrapper._testonly_._vars.get('var2').should.eql(var2);
+                wrapper._testonly_._initialVars.size.should.eql(2);
+                wrapper._testonly_._initialVars.get('var1').should.eql(var1);
+                wrapper._testonly_._initialVars.get('var2').should.eql(var2);
             });
         });
     });
