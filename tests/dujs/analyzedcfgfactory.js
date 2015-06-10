dadd5e0785e78b55605312f6cde7381c49b0e507
ChengFuLin <chengfulin0806@gmail.com>
2015-06-10 19:28:12 +0800

    Refactor to simply create empty a AnalyzedCFG

--- tests/dujs/analyzedcfgfactory.js
+++ tests/dujs/analyzedcfgfactory.js
@@ -18,18 +18,5 @@ describe('AnalyzedCFGFactory', function () {
             should.exist(analyzedCFG._testonly_._dupairs);
             analyzedCFG._testonly_._dupairs.length.should.eql(0);
         });
-
-        it('should support to create with ScopeWrapper', function () {
-            var node1 = factoryFlowNode.createNormalNode(),
-                node2 = factoryFlowNode.createNormalNode(),
-                cfg = [node1, node2, [node1, node2]],
-                wrapper = factoryScopeWrapper.create(cfg, Scope.PROGRAM_SCOPE),
-                analyzedCFG = factoryAnalyzedCFG.create(wrapper);
-
-            analyzedCFG._testonly_._scopeWrappers.length.should.eql(1);
-            should.exist(analyzedCFG._testonly_._cfg);
-            analyzedCFG._testonly_._cfg[0].should.eql(node1);
-            analyzedCFG._testonly_._cfg[1].should.eql(node2);
-        });
     });
 });
