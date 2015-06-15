da8a7a0636b688497d79d18a8c41bbffe3efb72b
ChengFuLin <chengfulin0806@gmail.com>
2015-06-15 21:13:21 +0800

    Refactor to read source then build scopes and intra-procedural CFGs

--- bin/dujs.js
+++ bin/dujs.js
@@ -4,19 +4,38 @@
 var fs = require('fs'),
     open = require('open'),
     dujs = require('../lib/dujs'),
+    graphics = require('../lib/dujs').graphics,
+    OUTPUT_DIR = 'out',
+    INTRA_PROCEDURAL_OUTPUTS_DIR = 'intra-procedurals',
+    INTER_PROCEDURAL_OUTPUTS_DIR = 'inter-procedurals',
+    INTRA_PAGE_OUTPUTS_DIR = 'intra-pages',
     filenames = [],
     sourceCode = '',
     analysisOutputs = null;
 
+/**
+ * Get source code from files
+ * @param files
+ * @param callback
+ * @function
+ */
 function getSourceFromFiles(files, callback) {
     "use strict";
     var source = '',
         currentFileName = '',
         readFileError = null;
+
+    /**
+     * Callback for reading a file
+     * @param err
+     * @param data
+     */
     function readFileCallback(err, data) {
-        if (!!err) { /// Failed
+        if (!!err) {
+            /// On error, log the failed file's name
             readFileError = new Error('Failed to read the file: ' + currentFileName);
-        } else { /// Succeed
+        } else {
+            /// On success, concat source code from files
             if (source !== '') {
                 source += '\n';
             }
@@ -41,28 +60,150 @@ function getSourceFromFiles(files, callback) {
     callback(readFileError, source);
 }
 
-process.argv.forEach(function (arg, index) {
-    "use strict";
-    if (index > 1) { /// as command line is "node bin/dujs.js [filename]"
-        filenames.push(arg);
-    }
-});
-
-function dujsCallback(err, outputs) {
-    "use strict";
-    if (!!err) {
-        throw err;
-    }
-    analysisOutputs = outputs;
-}
-
+/**
+ * Callback for read source code from files
+ * @param err
+ * @param data
+ * @function
+ */
 function getSourceCallback(err, data) {
     "use strict";
     if (!!err) {
         throw err;
     }
+    /// On success, do analysis
     sourceCode = data;
-    dujs(sourceCode, dujsCallback);
+    analysisOutputs = dujs(sourceCode);
 }
 
-getSourceFromFiles(filenames, getSourceCallback);
+/**
+ * Callback for make the directory for the output of intra-page analysis
+ * @param err
+ * @function
+ */
+function makeIntraPageOutputDirCallback(err) {
+    "use strict";
+    if (!!err) {
+        throw err;
+    }
+}
+
+/**
+ * Check the directory for the output of intra-page analysis is existed
+ * @param err
+ * @param stats
+ * @function
+ */
+function checkeIntraPageOutputDirCallback(err, stats) {
+    "use strict";
+    if (!!err) {
+        fs.mkdir(OUTPUT_DIR + '/' + INTRA_PAGE_OUTPUTS_DIR, makeIntraPageOutputDirCallback);
+    }
+}
+
+/**
+ * Callback for make the directory for the output of inter-procedural analysis
+ * @param err
+ * @function
+ */
+function makeInterProceduralOutputDirCallback(err) {
+    "use strict";
+    if (!!err) {
+        throw err;
+    }
+    fs.lstat(OUTPUT_DIR + '/' + INTRA_PAGE_OUTPUTS_DIR, checkeIntraPageOutputDirCallback);
+}
+
+/**
+ * Check the directory for the output of inter-procedural analysis is existed
+ * @param err
+ * @param stats
+ * @function
+ */
+function checkeInterProceduralOutputDirCallback(err, stats) {
+    "use strict";
+    if (!!err) {
+        fs.mkdir(OUTPUT_DIR + '/' + INTER_PROCEDURAL_OUTPUTS_DIR, makeInterProceduralOutputDirCallback);
+    }
+}
+
+/**
+ * Callback for make the directory for the output of intra-procedural analysis
+ * @param err
+ * @function
+ */
+function makeIntraProceduralOutputDirCallback(err) {
+    "use strict";
+    if (!!err) {
+        throw err;
+    }
+    fs.lstat(OUTPUT_DIR + '/' + INTER_PROCEDURAL_OUTPUTS_DIR, checkeInterProceduralOutputDirCallback);
+}
+
+/**
+ * Check the directory for the output of intra-procedural analysis is existed
+ * @param err
+ * @param stats
+ * @function
+ */
+function checkeIntraProceduralOutputDirCallback(err, stats) {
+    "use strict";
+    if (!!err) {
+        fs.mkdir(OUTPUT_DIR + '/' + INTRA_PROCEDURAL_OUTPUTS_DIR, makeIntraProceduralOutputDirCallback);
+    }
+}
+
+/**
+ * Callback for make the root directory for output
+ * @param err
+ * @function
+ */
+function makeOutputDirCallback(err) {
+    "use strict";
+    if (!!err) {
+        throw err;
+    }
+    fs.lstat(OUTPUT_DIR + '/' + INTRA_PROCEDURAL_OUTPUTS_DIR, checkeIntraProceduralOutputDirCallback);
+}
+
+/**
+ * Check the root directory of output is existed
+ * @param err
+ * @param stats
+ * @function
+ */
+function checkOutputDirCallback(err, stats) {
+    "use strict";
+    if (!!err) {
+        fs.mkdir(OUTPUT_DIR, makeOutputDirCallback);
+    }
+}
+
+/**
+ * Create output directories
+ */
+function createOutputDirectories() {
+    "use strict";
+    fs.lstat(OUTPUT_DIR, checkOutputDirCallback);
+}
+
+//*********************************************************************************
+/* Main */
+try {
+    /* Get user inputs */
+    process.argv.forEach(function (arg, index) {
+        "use strict";
+        if (index > 1) { /// as command line is "node bin/dujs.js [filename]"
+            filenames.push(arg);
+        }
+    });
+
+    /* Create otuput directories */
+    createOutputDirectories();
+
+    /* Get source codes */
+    getSourceFromFiles(filenames, getSourceCallback);
+
+} catch(err) {
+    console.log('Error');
+}
