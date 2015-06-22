861db30e0dfbf4654c1a5fe82b972a3354d0f998
ChengFuLin <chengfulin0806@gmail.com>
2015-06-22 21:35:22 +0800

    Test the method findGENSet of DefUseAnalyzer

--- tests/dujs/defuseanalyzer.js
+++ tests/dujs/defuseanalyzer.js
@@ -10,7 +10,7 @@ var analyzer = require('../../lib/dujs').DefUseAnalyzer,
 describe('DefUseAnalyzer', function () {
     "use strict";
     describe('Methods', function () {
-        describe('findKillSet', function () {
+        describe('findKILLSet', function () {
             describe('as for Single Variable Assignment', function () {
                 var cfg, scope;
                 beforeEach(function () {
@@ -132,6 +132,187 @@ describe('DefUseAnalyzer', function () {
                     var killSet = analyzer.findKILLSet(cfg[2][2]);
                     killSet.size.should.eql(1);
                     killSet.has(scope._testonly_._vars.get('a'));
+                });
+            });
+        });
+
+        describe('findGENSet', function () {
+            describe('as for Single Variable Declaration', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = 0;'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate VarDef at the node where the variable declared', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][1]);
+                    genSet.size.should.eql(1);
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
+                    genSet.values()[0]._testonly_._def._testonly_._type.should.eql('literal');
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._start.should.eql(8);
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._end.should.eql(9);
+                });
+            });
+
+            describe('as for Variable initialized with Assignment', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = 0;\n' +
+                        'var b = a = 1;'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate for the declared and redefined variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(2);
+                    /// it would generate from left to right
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('b'));
+                    genSet.values()[1]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
+                    genSet.values()[0]._testonly_._def.should.eql(genSet.values()[0]._testonly_._def);
+                });
+            });
+
+            describe('as for Variable initialized with Function Expression', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = function () {};'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate for the declared variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][1]);
+                    genSet.size.should.eql(1);
+                    genSet.values()[0]._testonly_._def._testonly_._type.should.eql('function');
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._start.should.eql(8);
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._end.should.eql(22);
+                });
+            });
+
+            describe('as for Single Assignment', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = 0;\n' +
+                        'a = 1;'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate for the redefined variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(1);
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._start.should.eql(15);
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._end.should.eql(16);
+                });
+            });
+
+            describe('as for Multiple Assignment', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = 0, b = 1;\n' +
+                        'a = b = 2;'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate for the redefined variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(2);
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._start.should.eql(26);
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._end.should.eql(27);
+                    genSet.values()[0]._testonly_._def.should.eql(genSet.values()[1]._testonly_._def);
+                });
+            });
+
+            describe('as for Assignment of Function Expression', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a;\n' +
+                        'a = function () {};'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate for the declared variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(1);
+                    genSet.values()[0]._testonly_._def._testonly_._type.should.eql('function');
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._start.should.eql(11);
+                    genSet.values()[0]._testonly_._def._testonly_._range._testonly_._end.should.eql(25);
+                });
+            });
+
+            describe('as for Update Expression', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    factoryFlowNode.resetCounter();
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = 0;\n' +
+                        '++a;\n'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate the definition of updated variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(1);
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
+                });
+            });
+
+            describe('as for Assignment of Update Expression', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    factoryFlowNode.resetCounter();
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = 0, b = 1;\n' +
+                        'a = b++;\n'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate the definition of assigned variable and the updated variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(2);
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
+                    genSet.values()[1]._testonly_._var.should.eql(scope._testonly_._vars.get('b'));
+                });
+            });
+
+            describe('as for Assignment of Member Expression', function () {
+                var cfg, scope;
+                beforeEach(function () {
+                    factoryFlowNode.resetCounter();
+                    cfg = cfgext.getCFG(cfgext.parseAST(
+                        'var a = {};\n' +
+                        'a.x = 1;\n'
+                    ));
+                    scope = factoryScopeWrapper.createProgramScopeWrapper(cfg);
+                    scope.setVars();
+                });
+
+                it('should generate the definition of object variable', function () {
+                    var genSet = analyzer.findGENSet(cfg[2][2]);
+                    genSet.size.should.eql(1);
+                    genSet.values()[0]._testonly_._var.should.eql(scope._testonly_._vars.get('a'));
                 });
             });
         });
