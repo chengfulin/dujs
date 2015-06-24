3a8f55a25bfabb2fb32ae7ed1b8e7f69efea9e9a
ChengFuLin <chengfulin0806@gmail.com>
2015-06-24 18:11:37 +0800

    Not test inter-procedurals yet

--- tests/dujs/analyzedcfgbuilder.js
+++ tests/dujs/analyzedcfgbuilder.js
@@ -4,9 +4,6 @@
 var builder = require('../../lib/dujs').analyzedCFGBuilder,
     CFGExt = require('../../lib/dujs').CFGExt,
     ScopeTree = require('../../lib/dujs').ScopeTree,
-    Scope = require('../../lib/dujs').Scope,
-    factoryScopeWrapper = require('../../lib/dujs').factoryScopeWrapper,
-    factoryFlowNode = require('../../lib/esgraph').factoryFlowNode,
     should = require('should');
 
 describe('AnalyzedCFGBuilder', function () {
@@ -47,7 +44,7 @@ describe('AnalyzedCFGBuilder', function () {
                 connectedCFG[2][2]._testonly_._prev[0].should.eql(connectedCFG[2][1]);
 
                 /// Return connection
-                connectedCFG[2][4]._testonly_.return.should.eql(connectedCFG[2][5]);
+                connectedCFG[2][4]._testonly_.return[0].should.eql(connectedCFG[2][5]);
                 connectedCFG[2][4]._testonly_._next.length.should.eql(1);
                 connectedCFG[2][4]._testonly_._next[0].should.eql(connectedCFG[2][5]);
                 connectedCFG[2][5]._testonly_._prev.length.should.eql(1);
@@ -82,37 +79,7 @@ describe('AnalyzedCFGBuilder', function () {
         });
 
         describe('buildInterProceduralCFGs', function () {
-            var entry1, exit1, entry2, exit2, normal1;
-            beforeEach(function () {
-                factoryFlowNode.resetCounter();
-                entry1 = factoryFlowNode.createEntryNode();
-                exit1 = factoryFlowNode.createExitNode();
-                entry2 = factoryFlowNode.createEntryNode();
-                exit2 = factoryFlowNode.createExitNode();
-                normal1 = factoryFlowNode.createNormalNode();
-
-                entry1._testonly_._cfgId = 0;
-                exit1._testonly_._cfgId = 1;
-                normal1._testonly_._cfgId = 3;
-                entry2._testonly_._cfgId = 4;
-                exit2._testonly_._cfgId = 5;
-
-                /// connection
-                entry1._testonly_.normal = normal1;
-                normal1._testonly_.normal = exit1;
-                entry2._testonly_.normal = exit2;
-            });
-
-            it('should produce correct AnalyzedCFG', function () {
-                var callerScope = factoryScopeWrapper.create([entry1, exit1, [entry1, normal1, exit1]], Scope.PROGRAM_SCOPE),
-                    calleeScope = factoryScopeWrapper.create([entry2, exit2, [entry2, exit2]], new Scope('foo'));
-                var interProceduralAnalyzedCFG = builder.buildInterProceduralCFG(callerScope, calleeScope, normal1);
-                should.exist(interProceduralAnalyzedCFG);
-                interProceduralAnalyzedCFG._testonly_._scopeWrappers.length.should.eql(2);
-                interProceduralAnalyzedCFG._testonly_._cfg[0].should.eql(entry1);
-                interProceduralAnalyzedCFG._testonly_._cfg[1].should.eql(exit1);
-                interProceduralAnalyzedCFG._testonly_._cfg[2].length.should.eql(6);
-            });
+            /// TODO: should be tested
         });
     });
 });
