ed4cdec105e255bdd9f4dfa84495b17d114bc3ec
ChengFuLin <chengfulin0806@gmail.com>
2015-07-27 11:55:54 +0800

    Refactor the simple factory for Var

--- tests/dujs/varfactory.js
+++ tests/dujs/varfactory.js
@@ -1,43 +1,21 @@
 /**
  * Created by ChengFuLin on 2015/5/13.
  */
-var Range = require('../../lib/dujs').Range,
-    Scope = require('../../lib/dujs').Scope,
-    varFactory = require('../../lib/dujs').factoryVar;
+var varFactory = require('../../lib/dujs/varfactory');
 require('should');
 
 describe('VarFactory', function () {
     'use strict';
     describe('create', function () {
         it('should create normal Var well', function () {
-            var normal = varFactory.create('normal', new Range(0,1), Scope.PROGRAM_SCOPE);
-            normal.toString().should.eql('normal@[0,1]_Program');
-        });
-    });
+            var normal = varFactory.create('normal');
+            normal._testonly_._name.should.eql('normal');
 
-    describe('createReturnVar', function () {
-        it('should create return Var well', function () {
-            var returned = varFactory.createReturnVar(new Range(0,1), Scope.PROGRAM_SCOPE);
-            returned.toString().should.eql('!RETURN@[0,1]_Program');
-        });
-    });
+	        var var123 = varFactory.create('var123');
+	        var123._testonly_._name.should.eql('var123');
 
-    describe('createDefaultReturnVar', function () {
-        it('should support to create default return Var', function () {
-            var defaultReturn = varFactory.createDefaultReturnVar(new Range(0,1), new Scope('foo'));
-            defaultReturn._testonly_._name.should.eql('!DEFAULT_RETURN');
-            defaultReturn._testonly_._range._testonly_._start.should.eql(0);
-            defaultReturn._testonly_._range._testonly_._end.should.eql(1);
-            defaultReturn._testonly_._scope._testonly_._value.should.eql('foo');
-        });
-    });
-
-    describe('createGlobalVar', function () {
-        it('should create global Var well', function () {
-            var global = varFactory.createGlobalVar('global'),
-                another = varFactory.createGlobalVar('another');
-            global.toString().should.eql('global@[0,0]_Global');
-            another.toString().should.eql('another@[0,0]_Global');
+	        var var_456 = varFactory.create('var_456');
+	        var_456._testonly_._name.should.eql('var_456');
         });
     });
 });
