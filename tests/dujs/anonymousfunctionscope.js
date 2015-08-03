00cc5b94b5dfcff756a82bc061376e9fc492090a
ChengFuLin <chengfulin0806@gmail.com>
2015-08-03 17:28:40 +0800

    Done testing AnonymousFunctionScope module

---
+++ tests/dujs/anonymousfunctionscope.js
@@ -0,0 +1,117 @@
+/*
+ * Test cases for AnonymousFunctionScope module
+ * @lastmodifiedBy ChengFuLin(chengfulin0806@gmail.com)
+ * @lastmodifiedDate 2015-08-30
+ */
+var should = require('should');
+var AnonymousFunctionScope = require('../../lib/dujs/anonymousfunctionscope');
+
+describe('AnonymousFunctionScope', function () {
+	"use strict";
+	beforeEach(function () {
+		AnonymousFunctionScope.numOfAnonymousFunctionScopes = 0;
+	});
+	describe('static data members', function () {
+		describe('numOfAnonymousFunctionScopes', function () {
+			it('should have correct default value', function () {
+				AnonymousFunctionScope.numOfAnonymousFunctionScopes.should.eql(0);
+			});
+
+			it('should support to be modified', function () {
+				AnonymousFunctionScope.numOfAnonymousFunctionScopes = 1;
+				AnonymousFunctionScope.numOfAnonymousFunctionScopes.should.eql(1);
+			});
+
+			it('should be enumerable', function () {
+				AnonymousFunctionScope.propertyIsEnumerable('numOfAnonymousFunctionScopes').should.eql(true);
+			});
+		});
+	});
+
+	describe('static methods', function () {
+		describe('resetCounter', function () {
+			it('should reset numOfAnonymousFunctionScopes well', function () {
+				AnonymousFunctionScope.numOfAnonymousFunctionScopes = 1;
+				AnonymousFunctionScope.resetCounter();
+				AnonymousFunctionScope.numOfAnonymousFunctionScopes.should.eql(0);
+			});
+		});
+
+		describe('validate', function () {
+			var validAST = {type: 'FunctionExpression', range: [0,1], loc: {line: 1, col: 0}};
+
+			it('should not throw as the inputs are valid', function () {
+				should(function () {
+					AnonymousFunctionScope.validate(validAST, '$ANONYMOUS_FUN_0', 'anonymousFunction', null);
+				}).not.throw();
+			});
+
+			it('should throw an error as the name of anonymous function scope is invalid', function () {
+				should(function () {
+					AnonymousFunctionScope.validate(validAST, 'invalid', 'anonymousFunction', null);
+				}).throw('Invalid value for an AnonymousFunctionScope');
+
+				should(function () {
+					AnonymousFunctionScope.validate(validAST, '$ANONYMOUS_FUN', 'anonymousFunction', null);
+				}).throw('Invalid value for an AnonymousFunctionScope');
+
+				should(function () {
+					AnonymousFunctionScope.validate(validAST, '$ANONYMOUS_FUN0', 'anonymousFunction', null);
+				}).throw('Invalid value for an AnonymousFunctionScope');
+			});
+
+			it('should throw an error as the AST is invalid', function () {
+				should(function () {
+					AnonymousFunctionScope.validate(
+						{type: 'Program', range: [0,1], loc: {line: 1, col: 0}},
+						'$ANONYMOUS_FUN_0',
+						'anonymousFunction',
+						null
+					);
+				}).throw('Invalid value for an AnonymousFunctionScope');
+			});
+
+			it('should throw an error as the type of scope is not an ANONYMOUS_FUN type', function () {
+				should(function () {
+					AnonymousFunctionScope.validate(validAST, '$ANONYMOUS_FUN_0', 'function', null);
+				}).throw('Invalid value for an AnonymousFunctionScope');
+			});
+
+			it('should throw an error as the parent of the socpe is invalid', function () {
+				should(function () {
+					AnonymousFunctionScope.validate(validAST, '$ANONYMOUS_FUN_0', 'anonymousFunction', {});
+				}).throw('Invalid value for an AnonymousFunctionScope');
+			});
+		});
+	});
+
+	describe('constructor', function () {
+		it('should increase the property "numOfAnonymousFunctionScope" and "index"', function () {
+			var ast = {type: 'FunctionExpression', range: [0,1], loc: {line: 1, col: 0}};
+			var scope = new AnonymousFunctionScope(ast, null);
+			scope._testonly_._index.should.eql(0);
+			AnonymousFunctionScope.numOfAnonymousFunctionScopes.should.eql(1);
+
+			var scope2 = new AnonymousFunctionScope(ast, null);
+			scope2._testonly_._index.should.eql(1);
+			AnonymousFunctionScope.numOfAnonymousFunctionScopes.should.eql(2);
+		});
+	});
+
+	describe('public data member', function () {
+		describe('index', function () {
+			var ast = {type: 'FunctionExpression', range: [0,1], loc: {line: 1, col: 0}};
+			var scope = new AnonymousFunctionScope(ast, null);
+
+			it('should not be modified', function () {
+				should(function () {
+					scope.index = 1;
+				}).throw();
+			});
+
+			it('should be enumerable', function () {
+				AnonymousFunctionScope.prototype.propertyIsEnumerable('index').should.eql(true);
+			});
+		});
+	});
+});
