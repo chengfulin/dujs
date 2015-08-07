df2f1279998c75e249360c9e13dbb9dbd1b3b0ea
ChengFuLin <chengfulin0806@gmail.com>
2015-08-07 16:58:27 +0800

    Refactor the counter of FlowNode instances

--- tests/esgraph/flownodetest.js
+++ tests/esgraph/flownodetest.js
@@ -1,12 +1,17 @@
-/**
- * Created by ChengFuLin on 2015/5/20.
+/*
+ * Test cases for FlowNode module
+ * @lastmodifiedBy ChengFuLin(chengfulin0806@gmail.com)
+ * @lastmodifiedDate 2015-08-07
  */
 var FlowNode = require('../../lib/esgraph/flownode'),
-    Set = require('../../lib/analyses').Set,
+    Set = require('../../lib/analyses/set'),
     should = require('should');
 
 describe('FlowNode', function () {
     "use strict";
+    beforeEach(function () {
+	    FlowNode.numOfNodes = 0;
+    });
 
     describe('constructor', function () {
         describe('create object with node type', function () {
@@ -43,10 +48,6 @@ describe('FlowNode', function () {
                 single = new FlowNode();
             });
             describe('default values', function () {
-                it('should not have cfgId yet', function () {
-                    should.not.exist(single._testonly_._cfgId);
-                });
-
                 it('should not have astNode', function () {
                     should.not.exist(single._testonly_._astNode);
                 });
