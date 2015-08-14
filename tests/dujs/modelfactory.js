f4ef68a267821b41409b60d1f3efe51e7a8cd588
ChengFuLin <chengfulin0806@gmail.com>
2015-08-14 18:03:36 +0800

    Refactor test cases for ModelFactory

--- tests/dujs/modelfactory.js
+++ tests/dujs/modelfactory.js
@@ -1,19 +1,23 @@
-/**
- * Created by ChengFuLin on 2015/6/10.
+/*
+ * Test cases for ModelFactory
+ * @lastmodifiedBy ChengFuLin(chengfulin0806@gmail.com)
+ * @lastmodifiedDate 2015-08-14
  */
-var factoryAnalyzedCFG = require('../../lib/dujs').factoryAnalyzedCFG,
-    should = require('should');
+var factoryModel = require('../../lib/dujs/modelfactory');
+var should = require('should');
 
 describe('ModelFactory', function () {
     "use strict";
-    describe('Factory Method', function () {
-        it('should support to create empty Model', function () {
-            var analyzedCFG = factoryAnalyzedCFG.create();
-            should.not.exist(analyzedCFG._testonly_._graph);
-            should.exist(analyzedCFG._testonly_._relatedScopes);
-            analyzedCFG._testonly_._relatedScopes.length.should.eql(0);
-            should.exist(analyzedCFG._testonly_._dupairs);
-            analyzedCFG._testonly_._dupairs.size.should.eql(0);
-        });
+    describe('public methods', function () {
+	    describe('create', function () {
+		    it('should support to create empty Model', function () {
+			    var model = factoryModel.create();
+			    should.not.exist(model._testonly_._graph);
+			    should.exist(model._testonly_._relatedScopes);
+			    model._testonly_._relatedScopes.length.should.eql(0);
+			    should.exist(model._testonly_._dupairs);
+			    model._testonly_._dupairs.size.should.eql(0);
+		    });
+	    });
     });
 });
