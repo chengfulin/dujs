cd0648b1bd0e584bfab1ca4c9cb273f60b340a43
ChengFuLin <chengfulin0806@gmail.com>
2015-08-19 14:30:09 +0800

    Refactor builder for intra-procedural model

--- tests/dujs/modelbuilder.js
+++ tests/dujs/modelbuilder.js
@@ -1,19 +1,17 @@
 /*
  * Test cases for ModelBuilder module
  * @lastmodifiedBy ChengFuLin(chengfulin0806@gmail.com)
- * @lastmodifiedDate 2015-08-07
+ * @lastmodifiedDate 2015-08-19
  */
-var should = require('should'),
-	esprima = require('esprima');
-var Model = require('../../lib/dujs/model'),
-	factoryScopeTree = require('../../lib/dujs/scopetreefactory'),
+require('should');
+var esprima = require('esprima');
+var scopeCtrl = require('../../lib/dujs/scopectrl'),
 	modelBuilder = require('../../lib/dujs/modelbuilder');
 
 describe('ModelBuilder', function () {
 	"use strict";
 	describe('public methods', function () {
 		describe('buildIntraProceduralModels', function () {
-			var scopeTree;
 			beforeEach(function () {
 				var ast = esprima.parse(
 					'var a, b;' +
@@ -33,37 +31,41 @@ describe('ModelBuilder', function () {
 					'fun();',
 					{range: true, loc: true}
 				);
-				scopeTree = factoryScopeTree.create();
-				scopeTree.buildScopeTree(ast);
+				scopeCtrl.addPageScopeTree(ast);
+			});
+
+			afterEach(function () {
+				scopeCtrl.clear();
 			});
 
 			it('should contain correct number of models', function () {
-				var models = modelBuilder.buildIntraProceduralModels(scopeTree);
-				models.length.should.eql(4);
+				var modelsOfPages = modelBuilder.buildIntraProceduralModels();
+				modelsOfPages.length.should.eql(1);
+				modelsOfPages[0].length.should.eql(4);
 			});
 
 			it('should contain corresponding scope', function () {
-				var models = modelBuilder.buildIntraProceduralModels(scopeTree);
-				models.forEach(function (model) {
+				var modelsOfPages = modelBuilder.buildIntraProceduralModels();
+				modelsOfPages[0].forEach(function (model) {
 					model._testonly_._relatedScopes.length.should.eql(1);
 				});
-				scopeTree._testonly_._scopes.every(function (scope) {
-					return models.some(function (model) {
+				scopeCtrl.pageScopeTrees[0]._testonly_._scopes.every(function (scope) {
+					return modelsOfPages[0].some(function (model) {
 						return model._testonly_._relatedScopes.indexOf(scope) !== -1;
 					});
 				}).should.eql(true);
 			});
 
 			it('should contain corresponding graph', function () {
-				var models = modelBuilder.buildIntraProceduralModels(scopeTree);
-				models.forEach(function (model) {
-					if (model._testonly_._mainlyRelatedScope === scopeTree._testonly_._scopes[0]) {
+				var modelsOfPages = modelBuilder.buildIntraProceduralModels();
+				modelsOfPages[0].forEach(function (model) {
+					if (model._testonly_._mainlyRelatedScope === scopeCtrl.pageScopeTrees[0]._testonly_._scopes[0]) {
 						model._testonly_._graph[2].length.should.eql(5);
-					} else if (model._testonly_._mainlyRelatedScope === scopeTree._testonly_._scopes[1]) {
+					} else if (model._testonly_._mainlyRelatedScope === scopeCtrl.pageScopeTrees[0]._testonly_._scopes[1]) {
 						model._testonly_._graph[2].length.should.eql(4);
-					} else if (model._testonly_._mainlyRelatedScope === scopeTree._testonly_._scopes[2]) {
+					} else if (model._testonly_._mainlyRelatedScope === scopeCtrl.pageScopeTrees[0]._testonly_._scopes[2]) {
 						model._testonly_._graph[2].length.should.eql(6);
-					} else if (model._testonly_._mainlyRelatedScope === scopeTree._testonly_._scopes[3]) {
+					} else if (model._testonly_._mainlyRelatedScope === scopeCtrl.pageScopeTrees[0]._testonly_._scopes[3]) {
 						model._testonly_._graph[2].length.should.eql(3);
 					}
 				});
